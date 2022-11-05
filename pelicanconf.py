2fd2a3b5b8fc552fa76dbf296e0b632d576ee2d1
Carlos Carrasco Varas <matrix.carlos@gmail.com>
2022-11-04 22:57:20 -0300

    :test_tube: probando los links laterales

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,7 +16,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
+PAGES = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
          ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
          ('You can modify those links in your config file', '#'),)
